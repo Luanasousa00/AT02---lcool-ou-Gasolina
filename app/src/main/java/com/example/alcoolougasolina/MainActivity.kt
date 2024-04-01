ea18d368129c52d499641a7e8d03acd3936f924c
Luana Sousa <luhsousa00@gmail.com>
2024-04-01 10:44:03 -0300

    correção de erros

--- app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.example.alcoolougasolina
 
 import android.annotation.SuppressLint
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Switch
